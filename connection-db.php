fd56dd0f3cd0c903057eff605f8656c4f7d9c62c
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2022-12-20 21:56:07 +0100

    started with db-connection, added images for the rooms

---
+++ connection-db.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/hotelFunctions.php';
+
+$db = connect('/hotel.db');
+
+// $statement = $db->query('SELECT * FROM reservations');
+
+// $reservations = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+if (isset($_POST['fName'], $_POST['lName'], $_POST['transferCode'], $_POST['dateArraving'], $_POST['dateLeaving'], $_POST['rooms'])) {
+                    $fname = trim(htmlspecialchars($_POST['fName'], ENT_QUOTES));
+                    $lname = trim(htmlspecialchars($_POST['lNname'], ENT_QUOTES));
+                    $transferCode = trim(htmlspecialchars($_POST['transferCode'], ENT_QUOTES));
+                    $dateArraving = trim(htmlspecialchars($_POST['dateArraving'], ENT_QUOTES));
+                    $dateLeaving = trim(htmlspecialchars($_POST['dateLeaving'], ENT_QUOTES));
+                    $rooms = trim(htmlspecialchars($_POST['rooms'], ENT_QUOTES));
+
+                    // If dates abd room are available and if transfer code is valid
+
+                    $query = 'INSERT INTO reservations (f_name, l_name, transfer_code, date_arraving, date_leaving, room_id) VALUES (:f_name, :l_name, :transfer_code, :date_arraving, :date_leaving, :room_id)';
+
+                    $statement = $db->prepare($query);
+
+                    $statement->bindParam(':f_name', $fname, PDO::PARAM_STR);
+                    $statement->bindParam(':l_name', $lname, PDO::PARAM_STR);
+                    $statement->bindParam(':transfer_code', $transferCode, PDO::PARAM_INT);
+                    $statement->bindParam(':date_arraving', $dateArraving, PDO::PARAM_STR);
+                    $statement->bindParam(':date_leaving', $dateLeaving, PDO::PARAM_STR);
+                    $statement->bindParam(':room_id', $rooms, PDO::PARAM_INT);
+
+                    $statement->execute();
+}
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 require __DIR__ . '/calender.php';
+require __DIR__ . '/connection-db.php';
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,52 +12,104 @@ require __DIR__ . '/calender.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="main.css">
-
     <link rel="icon" href="/images/tree.png">
-
     <title>Albero</title>
 </head>
 
 <body>
     <header>
+        <nav>
+
+
+        </nav>
+
     </header>
     <main>
-        <h1> Tree Hotel wow!</h1>
+        <section class="room-section">
+            <section class="room-one">
+                <h2>Cheapskate</h2>
+                <img src="/images/treehousebad.jpeg" width="500px" alt="cheap tree house, just some planks in a tree">
+                <p> Heres a room for the one with not as much cash in their pockets, who still wants to enjoy a
+                    night or more in the tree-tops. You get acces to a roof, walls, floor and even a window! Theres
+                    no room to lie down but we have made sure there is a nice and comfy pillow for you to sit on!
 
-        <h2>Cheapskate</h2>
-        <h2> Budget-queen</h2>
-        <h2>Luxury</h2>
+                    How do I get up you there you might think? You just climb. </p>
 
-        <section class="calander">
-            <h3>Select what room you want:</h2>
-                <select name="room" id="room" class="form-input">
+            </section>
+            <section class="room-two">
+                <h2> Budget-queen</h2>
+                <img src="/images/treehousebudget.jpeg">
+                <p>Wanna get some comfort and a nice bed to sleep in? Our budget room is for you!</p>
 
-                    <option value="1">Cheapskate</option>
-                    <option value="2">Budget-queen</option>
-                    <option value="3">Luxury</option>
-                </select> <br><br>
-                <?php echo $calendar->draw(date('2023-01-01')) ?>
-                <form method="POST" action="index.php">
-                    <label for="fname">First name:</label>
-                    <input type="text" id="fname" name="fname" class="form-input">
-                    <label for="lname">Last name:</label>
-                    <input type="text" id="lname" name="lname" class="form-input">
-                    <label for="transferCode"> Transfer Code:</label>
-                    <input type="text" id="transferCode" name="transferCode">
-                    <label for="arriving">Date of arrival:</label>
-                    <input type="date" id="dateArraving" name="dateArraving" class="form-input" max="2023-01-31"
-                        min="2023-01-01">
-                    <label for="leaving">Date of departure:</label>
-                    <input type="date" id="dateDeparture" name="dateDeparture" class="form-input" max="2023-01-31"
-                        min="2023-01-01">
-                    <input type="submit" value="submit">
+            </section>
 
-                </form>
-        </section>
+
+            <section class="room-three">
+                <h2>Luxury</h2>
+                <img src="/images/treehouselux1.jpeg">
+                <p>You got some cash and you aint afraid to spend it! Our luxury option gives you more space then you
+                    probably need. </p>
+            </section>
+            <section class="calander">
+                <h3>Select what room you want:</h2>
+                    <select name="rooms" id="rooms" class="form-input">
+                        <option value="1">Cheapskate</option>
+                        <option value="2">Budget-queen</option>
+                        <option value="3">Luxury</option>
+                        <?php echo $calendar->draw(date('2023-01-01')) ?>
+                    </select> <br><br>
+
+                    <form method="POST" action="/index.php">
+                        <label for="f_name">First name:</label>
+                        <input type="text" id="f_name" name="f_name" class="form-input">
+                        <label for="l_name">Last name:</label>
+                        <input type="text" id="l_name" name="l_name" class="form-input">
+                        <label for="transferCode"> Transfer Code:</label>
+                        <input type="text" id="transferCode" name="transferCode">
+                        <label for="dateArraving">Date of arrival:</label>
+                        <input type="date" id="dateArraving" name="dateArraving" class="form-input" max="2023-01-31"
+                            min="2023-01-01">
+                        <label for="dateLeaving">Date of departure:</label>
+                        <input type="date" id="dateLeaving" name="dateLeaving" class="form-input" max="2023-01-31"
+                            min="2023-01-01">
+                        <input type="submit" value="submit">
+
+                    </form>
+            </section>
     </main>
     <footer>
     </footer>
 </body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
