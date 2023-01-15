f1936b6159be2170410eefb810e59abbc3d8c9ec
hedlundemma <112821036+hedlundemma@users.noreply.github.com>
2023-01-15 18:16:19 +0100

    fixed pathways for deploy

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require __DIR__ . '/calender.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style/main.css">
-
+    <link rel="stylesheet" type="text/css" href="style/header.css">
     <link rel="icon" href="images/tree.png">
 
     <title>Albero</title>
